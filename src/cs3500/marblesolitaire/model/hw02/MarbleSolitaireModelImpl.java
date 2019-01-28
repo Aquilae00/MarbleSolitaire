da88a1175331f1b442425bcd3b7143c3bdfa3391
Test <linaldi.b@husky.neu.edu>
2019-01-27 19:14:54 -0500

    code move

--- src/cs3500/marblesolitaire/model/hw02/MarbleSolitaireModelImpl.java
+++ src/cs3500/marblesolitaire/model/hw02/MarbleSolitaireModelImpl.java
@@ -1,10 +1,12 @@
 package cs3500.marblesolitaire.model.hw02;
 
+import java.util.Arrays;
+
 /**
  * Class implementation of MarbleSolitaireModel
  */
 public final class MarbleSolitaireModelImpl implements MarbleSolitaireModel {
-  private String[][] board;
+
   private final int DEFAULT_ARM = 3;
 
   public MarbleSolitaireModelImpl() {
@@ -18,6 +20,10 @@ public final class MarbleSolitaireModelImpl implements MarbleSolitaireModel {
   public MarbleSolitaireModelImpl(int thicc) {
 
   }
+
+  public MarbleSolitaireModelImpl(int thicc, int row, int col) {
+
+  }
   @Override
   public void move(int fromRow, int fromCol, int toRow, int toCol) throws IllegalArgumentException {
 
@@ -28,17 +34,79 @@ public final class MarbleSolitaireModelImpl implements MarbleSolitaireModel {
     return false;
   }
 
-  @Override
-  public String getGameState() {
-    int width = DEFAULT_ARM * 4 + 1;
-    int boardSize = width * width;
-    for (int x = 0; x <  boardSize - 1; ++x) {
-      for (int y = 0; y < boardSize - 1; ++y) {
-        this.board[x][y] = "0";
+  /**
+   * Initialize the board in the form of int arrays.
+   * <table>
+   *   <thead>
+   *     <td>Number</td>
+   *     <td>String</td>
+   *   </thead>
+   *   <tr><td>0</td><td>Represents " "</td></tr>
+   *   <tr><td>1</td><td>Represents "O"</td></tr>
+   *   <tr><td>2</td><td>Represents "_"</td></tr>
+   *   <tr><td>3</td><td>Represents "" (Nothing)</td></tr>
+   * </table>
+   *
+   * @return the board in form of int arrays
+   */
+  public int[][] initBoard() {
+    int h_gap = DEFAULT_ARM - 1;
+    int front_gap = h_gap * 2;
+    int width = front_gap * 2 + h_gap + DEFAULT_ARM;
+    int last_slot = width - front_gap;
+
+    int v_gap = DEFAULT_ARM - 1;
+    int height = v_gap * 2 + DEFAULT_ARM;
+    int[][] board = new int[height][width];
+
+    for (int x = 0; x < width ; ++x) {
+      for (int y = 0; y < height; ++y) {
+        if (y < v_gap || y >= height - v_gap) {
+          if ((x >= front_gap) && (x % 2 == 0) && x < last_slot) {
+            board[y][x] = 1;
+          }
+          else if((x >= front_gap) && !(x % 2 == 0) && x < last_slot) {
+            board[y][x] = 0;
+          }
+          else if (x >= last_slot) {
+            board[y][x] = 3;
+          }
+        }
+         else  {
+           if (x % 2 == 0) {
+             board[y][x] = 1;
+           }
+           else {
+             board[y][x] = 0;
+           }
+        }
       }
     }
-    return this.board.toString();
+    return board;
+  }
 
+  @Override
+  public String getGameState() {
+    int[][] init_board = this.initBoard();
+    StringBuilder string_board = new StringBuilder();
+    for (int[] iArray : init_board) {
+      for (int i : iArray) {
+        switch(i) {
+          case 0 :
+            string_board.append(" ");
+            break;
+          case 1 :
+            string_board.append("O");
+            break;
+          case 2 :
+            string_board.append("_");
+            break;
+
+        }
+      }
+      string_board.append("\n");
+    }
+    return string_board.toString();
   }
 
   @Override
--- test/MarbleSolitaireModelImplTest.java
+++ test/MarbleSolitaireModelImplTest.java
@@ -1,2 +1,16 @@
+import org.junit.Test;
+
+import java.util.Arrays;
+
+import static org.junit.Assert.assertEquals;
+
+import cs3500.marblesolitaire.model.hw02.MarbleSolitaireModelImpl;
+
 public class MarbleSolitaireModelImplTest {
+  MarbleSolitaireModelImpl m0 = new MarbleSolitaireModelImpl();
+
+  @Test
+  public void testMove() {
+    System.out.println(m0.getGameState());
+  }
 }
